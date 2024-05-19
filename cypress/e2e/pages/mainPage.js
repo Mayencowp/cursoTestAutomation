9d892df17dfbe361e60deaa251d0228e249868e6
Javier Flores <102623042+JavierFB83@users.noreply.github.com>
2024-05-19 20:49:00 +0200

    Update mainPage.js
      Añade más funciones por ejemplo alquna que compruebe que aparece algunos elementos en la página o alguna que haga type

--- cypress/e2e/pages/mainPage.js
+++ cypress/e2e/pages/mainPage.js
@@ -4,4 +4,6 @@ export class MainPage{
         cy.get(`#${linkName}`).should('be.visible').click();
     }
 
+    // Añade más funciones por ejemplo alquna que compruebe que aparece algunos elementos en la página o alguna que haga type
+
 }
